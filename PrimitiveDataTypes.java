f9d58033b88794670521d7257a3133dae51428c3
geekofcodes <madhureddy2019cse@gmail.com>
2022-12-29 11:24:19 +0530

    created java programs

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,58 @@
+public class PrimitiveDataTypes {
+
+    public static void main(String[] args){
+
+        /* Integer data type */
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MAX_VALUE;
+        System.out.println("Integer Min value: " + minValue);
+        System.out.println("Integer Max Value: " + maxValue);
+
+        /* Short data type */
+        short shortMinValue = Short.MIN_VALUE;
+        short shortMaxValue = Short.MAX_VALUE;
+        System.out.println("Short Min value: " + shortMinValue);
+        System.out.println("Short Max value: " + shortMaxValue);
+
+        /* Byte data type */
+        byte byteMinValue = Byte.MIN_VALUE;
+        byte byteMaxValue = Byte.MAX_VALUE;
+        System.out.println("Byte Min value: " + byteMinValue);
+        System.out.println("Byte Max value: " + byteMaxValue);
+
+        /* Long data type */
+        long longMaxValue = Long.MIN_VALUE;
+        long longMinValue = Long.MAX_VALUE;
+        System.out.println("Long Min value: " + longMinValue);
+        System.out.println("Long Max value: " + longMaxValue);
+
+        /* Float data type */
+        float floatMaxValue = Float.MIN_VALUE;
+        float floatMinValue = Float.MAX_VALUE;
+        System.out.println("Float Min value: " + floatMinValue);
+        System.out.println("Float Max value: " + floatMaxValue);
+
+        /* Double data type */
+        double doubleMinValue = Long.MIN_VALUE;
+        double doubleMaxValue = Long.MAX_VALUE;
+        System.out.println("Double Min value: " + doubleMinValue);
+        System.out.println("Double Max value: " + doubleMaxValue);
+
+        /* Char data type
+        Char represents a single character.
+        It is defined in java program using single quote ('a')
+         */
+
+        char myChar1 = 'A';
+        char myChar2 = '1';
+        System.out.println("Character data type variable 1 value: " + myChar1);
+        System.out.println("Character data type variable 2 value: " + myChar2);
+
+        /* Boolean data type
+        Boolean data type contains values true/ false or 0/1.
+         */
+        boolean IamAbove18 = true;
+        System.out.println(IamAbove18);
+    }
+
+}
